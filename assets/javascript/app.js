587c9a5d270e26bafe1a87e902312bcc07c5d705
Hernavch <hernandezvc1292@gmail.com>
2019-01-19 14:49:06 -0800

    Update to array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,55 @@
+var trivia = [
+    {hint:"Here is your question" ,
+    answer:"Here is the answer",
+    options:["Here is a possible",  "Here is a possible", "Here is a possible"]
+    },
 
+     {hint:"Here is your question" ,
+    answer:"Here is the answer",
+    options:["Here is a possible",  "Here is a possible", "Here is a possible"]
+    },
+    {hint:"Here is your question" ,
+    answer:"Here is the answer",
+    options:["Here is a possible",  "Here is a possible", "Here is a possible"]
+        }
+
+];
+
+console.log(trivia[0].hint);
+
+// var correctQ1 = trivia.trivia1.answers;
+// var correctQ2 = trivia.trivia2.answers;
+// var correctQ3 = trivia.trivia3.answers;
+
+// for(var i = 0 ; i < trivia.length ; i ++ ) {
+//     var question = trivia[i].q;
+//     for(var j = 0 ; j < trivia[i].options.length ; j++) {
+//         $('.choices').append('<button>'+trivia[i].options[j] + '/button')
+//     }
+
+// }
+
+
+// if($(this).text() == trivia[i].answer) {
+//     score
+// }
+
+
+
+console.log(trivia.trivia1);
 
 $("#startbtn").on("click", function(){
-    alert("it worked");
+    for(var i = 0 ; i < trivia.length ; i ++ ) {
+        var question = trivia[i].q;
+        for(var j = 0 ; j < trivia[i].options.length ; j++) {
+            $('.choices').append('<button>'+trivia[i].options[j] + '/button')
+        }
+    
+    }
+    console.log(hint1);
+
+    // $(".board").html();
+    $(".board").empty();
+    $(".board").html(question1);
+    $(".question1").append('<p>'+  +'</p>')
 })
