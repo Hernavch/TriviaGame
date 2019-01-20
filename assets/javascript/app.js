6151706e4b7cba1562df7ebcf72e0e5cb55334f3
Hernavch <hernandezvc1292@gmail.com>
2019-01-20 13:16:55 -0800

    Attempt to append2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,28 @@
 var trivia = [
     {hint:"Here is your question 1" ,
-    answer:"Here is the answer",
+    answer:"Here is the answer Q1",
     options:["Here is a possible 1",  "Here is a possible 2"]
     },
 
     {hint:"Here is your question 2" ,
-    answer:"Here is the answer",
+    answer:"Here is the answer Q2",
     options:["Here is a possible1.1",  "Here is a possible 1.2"]
     },
     {hint:"Here is your question 3" ,
-    answer:"Here is the answer",
+    answer:"Here is the answer Q3",
     options:["Here is a possible2.1",  "Here is a possible2.2"]
         }
 
 ];
 
+// console.log(trivia[0].hint);
+
+
 // if($(this).text() == trivia[i].answer) {
 //     score
 // }
+
+
 
 $("#startbtn").on("click", function(){
     $(".board").empty();
@@ -28,39 +33,33 @@ $("#startbtn").on("click", function(){
         var question = trivia[i].hint;
         // console.log(question);
         console.log(trivia[i].options.length);
-        
-        $(".board").append('<div class="questions">' + question + '<div>');
-
-            for (var j = 0; j < 2; j++) {
-                var choices = trivia[i].options[j];
-                // var choices1 = trivia[0].options[j];
-                // var choices2 = trivia[1].options[j];
-                // var choices3 = trivia[2].options[j];
-                console.log(choices);
-                               
-                // console.log(choices2)
-                // $(".board").append('<div class="questions">' + question + '<div>');
-                $(".questions").append('<button class="choosebtn" id="q1">'+ choices + '</button>')
-                // $(".questions").append()
-
-                // console.log(trivia[i].answer);
-                // console.log(trivia[i].options[j]);
-
-                // $(".questions").append('<p>'+ choices +'</p>');
-
-            // }
-
-        // $(".questions").append('<p>'+ choices +'</p>');
-
-        // for(var j = 0 ; j < trivia[i].options.length ; j++) {
-        //     $('.choices').append('<button>'+trivia[i].options[j] + trivia[i].answer + '</button>')
-        // }
     
-    }
-    }
+        $(".board").append('<div class="questions">' + question + '<div>');
+    
+
+            for (var j = 0; j < trivia[i].options.length; j++) {
+                console.log(trivia[i].options[j]);
+                var choices = trivia[i].options[j];
+            //     // var choices1 = trivia[0].options[j];
+            //     // var choices2 = trivia[1].options[j];
+            //     // var choices3 = trivia[2].options[j];
+            //     console.log(choices);
+                    // if(trivia[i].options[j]> 2){
+                        $(".choosebtn").empty()
+                    }
+                    // $(".questions").append('<button class="choosebtn" id="q1">'+ choices + '</button>');
+                    $(".questions").append('<button class="choosebtn answer" id="answer1">' + trivia[0].answer + '</button>');
+                    $(".questions").append('<button class="choosebtn answer" id="answer2">' + trivia[1].answer + '</button>');
+                    $(".questions").append('<button class="choosebtn answer" id="answer3">' + trivia[2].answer + '</button>');
+                    
+                }            
+    });
+    // $(".questions").append('<button class="choosebtn" id="q1">'+ choices + '</button>');
+    // $(".questions").append('<button class="choosebtn">' + trivia[i].answer + '</button>');
+    // }
    
     // $(".board").html();
     // $(".board").empty();
     // $(".board").html(question1);
     // $(".question1").append('<p>'+  +'</p>')
-});
+// });
